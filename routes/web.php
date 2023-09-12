ba960a1a8293e81912177968f79d4704727524de
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-09-12 15:05:04 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,13 @@ Route::get('/', function () {
 Route::fallback(function(){
     return view('not_found_page');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
